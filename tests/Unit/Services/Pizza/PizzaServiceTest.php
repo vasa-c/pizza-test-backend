8ecb2aac27ea074220682ffe043e16f97006c4fe
Oleg Grigoriev <go.vasac@gmail.com>
2019-12-22 01:04:39 +0300

    PizzaService::parseCart() now returns array OrderItem[]

--- tests/Unit/Services/Pizza/PizzaServiceTest.php
+++ tests/Unit/Services/Pizza/PizzaServiceTest.php
@@ -8,6 +8,7 @@ use Tests\TestCase;
 use App\{
     ServiceContainer,
     PizzaType,
+    OrderItem
 };
 
 class PizzaServiceTest extends TestCase
@@ -49,5 +50,25 @@ class PizzaServiceTest extends TestCase
                 'photo' => 'http://pizza.loc/assets/img/pizza/two.png',
             ],
         ], ServiceContainer::pizza()->getDataForList());
+    }
+
+    public function testParseCart(): void
+    {
+        $this->migrate();
+        $this->assertNull(ServiceContainer::pizza()->parseCart([
+            'chicago' => 3,
+            'greek' => 2,
+            'xxx' => 1,
+        ]), 'unknown pizza');
+        $items = ServiceContainer::pizza()->parseCart([
+            'chicago' => 3,
+            'greek' => 2,
+        ]);
+        $this->assertCount(2, $items);
+        $chicago = $items['chicago'];
+        $this->assertInstanceOf(OrderItem::class, $chicago);
+        $this->assertSame('Chicago', $chicago->getPizza()->name);
+        $this->assertSame(3, $chicago->count);
+        $this->assertEquals($chicago->getPizza()->price, $chicago->item_price);
     }
 }
