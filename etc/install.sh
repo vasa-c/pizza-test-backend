8cf5435c273a7709f1b487cd11d4143297d1b656
Oleg Grigoriev <go.vasac@gmail.com>
2019-12-21 19:04:55 +0300

    Install: install & deploy scripts, chmod and doc

---
+++ etc/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")/.." \
+&& composer install --ignore-platform-reqs \
+&& chmod 777 storage/framework/views \
+&& touch storage/logs/laravel.log \
+&& chmod 666 storage/logs/laravel.log \
+&& ./artisan install:nginx \
+&& ./artisan migrate
