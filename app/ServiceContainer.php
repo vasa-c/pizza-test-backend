32daf8be19c415c0547e23ea238a3ed448911fa6
Oleg Grigoriev <go.vasac@gmail.com>
2019-12-21 19:48:40 +0300

    Pizza: register PizzaService

--- app/ServiceContainer.php
+++ app/ServiceContainer.php
@@ -5,11 +5,17 @@ declare(strict_types=1);
 namespace App;
 
 use App\Services\Users\IUsersService;
+use App\Services\Pizza\IPizzaService;
 
 class ServiceContainer
 {
     public static function users(): IUsersService
     {
         return app()->make(IUsersService::class);
+    }
+
+    public static function pizza(): IPizzaService
+    {
+        return app()->make(IPizzaService::class);
     }
 }
