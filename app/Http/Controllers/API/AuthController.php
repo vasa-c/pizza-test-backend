a7d0b92c542fb49c83a3e365acef79e291d7ab5b
Oleg Grigoriev <go.vasac@gmail.com>
2019-12-21 22:47:57 +0300

    API: login

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,12 +4,25 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\API;
 
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use RuntimeException;
 
 class AuthController extends APIController
 {
+    public function login(LoginRequest $request)
+    {
+        $user = $this->getCurrentUser();
+        if ($user === null) {
+            $this->attemptLogin($request->email, $request->password);
+        }
+        $user = $this->getCurrentUser();
+        return response()->json([
+            'user' => $user ? $user->getDataForFrontend() : null,
+        ]);
+    }
+
     public function logout(Request $request)
     {
         if ($this->getCurrentUser()) {
@@ -21,5 +34,18 @@ class AuthController extends APIController
             }
         }
         return response()->json([]);
+    }
+
+    /**
+     * @param string $email
+     * @param string $password
+     */
+    private function attemptLogin(string $email, string $password): void
+    {
+        $credentials = [
+            'email' => $email,
+            'password' => $password,
+        ];
+        Auth::guard()->attempt($credentials, true);
     }
 }
--- tests/Feature/Http/API/AuthTest.php
+++ tests/Feature/Http/API/AuthTest.php
@@ -10,6 +10,47 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthTest extends TestCase
 {
+    public function testLogin(): void
+    {
+        $this->migrate();
+        /** @var User $user1 */
+        $user1 = factory(User::class)->make();
+        $user1->email = 'one@example.com';
+        $user1->setPassword('one');
+        $user1->save();
+        /** @var User $user2 */
+        $user2 = factory(User::class)->make();
+        $user2->email = 'two@example.com';
+        $user2->setPassword('two');
+        $user2->save();
+        // invalid request
+        $this->postJson('/api/login', ['xxx' => 3])->assertStatus(422);
+        // wrong password
+        $response = $this->postJson('/api/login', [
+            'email' => 'one@example.com',
+            'password' => 'two',
+        ]);
+        $response->assertStatus(200);
+        $this->assertNull(Auth::user());
+        $this->assertNull($response->json()['user']);
+        // success
+        $response = $this->postJson('/api/login', [
+            'email' => 'one@example.com',
+            'password' => 'one',
+        ]);
+        $response->assertStatus(200);
+        $this->assertTrue($user1->is(Auth::user()));
+        $this->assertEquals($user1->getDataForFrontend(), $response->json()['user']);
+        // already logined
+        $response = $this->postJson('/api/login', [
+            'email' => 'two@example.com',
+            'password' => 'two',
+        ]);
+        $response->assertStatus(200);
+        $this->assertTrue($user1->is(Auth::user()));
+        $this->assertEquals($user1->getDataForFrontend(), $response->json()['user']);
+    }
+
     public function testLogout(): void
     {
         $this->migrate();
