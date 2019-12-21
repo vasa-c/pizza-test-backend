bd6527b98aeddee1c1254a72e18b50c3022450d3
Oleg Grigoriev <go.vasac@gmail.com>
2019-12-21 16:39:47 +0300

    Config: example env file and config.pizza section

---
+++ config/pizza.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'adminEmail' => env('ADMIN_EMAIL'),
+    'generatePasswordAsEmail' => env('GENERATE_PASSWORD_AS_EMAIL', false),
+    'nginx' => [
+        'listen' => env('NGINX_LISTEN', '80'),
+        'nuxt' => env('NGINX_NUXT_PROXY', 'http://localhost:3000'),
+        'fpm' => env('NGINX_FPM', 'php-fpm'),
+    ],
+];
