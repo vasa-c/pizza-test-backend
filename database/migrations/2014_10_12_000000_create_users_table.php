a2f18842ccd562b075085d70ea605b4c0acab4e9
Oleg Grigoriev <go.vasac@gmail.com>
2019-12-21 20:09:20 +0300

    Price class

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Price;
 
 class CreateUsersTable extends Migration
 {
@@ -16,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('address')->default('');
             $table->string('contacts')->default('');
-            $table->string('currency')->default('eur');
+            $table->string('currency')->default(Price::getDefaultCurrency());
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
